a3893ba1cb730adefd3dd7b0a30c9211ec9a58a8
izzeddinayyad <azeedenayyad@gmail.com>
2023-09-16 00:28:22 +0300

    commit

---
+++ scrip.js
@@ -0,0 +1,13 @@
+var sum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+let nums = [2,7,3,11,15];
+let target =10;
+console.log(sum(nums, target)); 
